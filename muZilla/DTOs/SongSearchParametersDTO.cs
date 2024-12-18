b8cda92a22608665ab8e31af7604b6c7a9e7cb4b
Timbuhtuk <timpfoy@gmail.com>
2024-12-18 17:53:30 +0200

    SearchSong method now support releaze date as parameter

---
+++ muZilla/DTOs/SongSearchParametersDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace muZilla.DTOs
+{
+    public class SongSearchParametersDTO
+    {
+        public string? Title { get; set; }
+        public string? Genres { get; set; }
+        public bool? HasExplicit { get; set; }
+        public DateTime? FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+    }
+}
